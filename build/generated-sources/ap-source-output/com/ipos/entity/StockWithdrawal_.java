b37acfda02bbb529de003b06b558a29a848d27b8
Mikho Anthony Malto <Mikho Anthony Malto>
2017-05-26 23:40:09 +0800

    Added git ignore.

--- build/generated-sources/ap-source-output/com/ipos/entity/StockWithdrawal_.java
+++ build/generated-sources/ap-source-output/com/ipos/entity/StockWithdrawal_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-26T22:45:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-05-26T23:31:15")
 @StaticMetamodel(StockWithdrawal.class)
 public class StockWithdrawal_ { 
 
