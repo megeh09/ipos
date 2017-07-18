77ca80e9bad7399d5d48af35e4943f3122366c0a
Mikho Anthony Malto <Mikho Anthony Malto>
2017-07-18 18:32:27 +0800

    For deployment, fixed table incremental search.

--- build/generated-sources/ap-source-output/com/ipos/entity/Sales_.java
+++ build/generated-sources/ap-source-output/com/ipos/entity/Sales_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-18T18:08:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-18T18:31:49")
 @StaticMetamodel(Sales.class)
 public class Sales_ { 
 
